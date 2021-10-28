60feccece27372b3c231ae8c6897b8bc02945cfa
elkhayate <melkhayate61@gmail.com>
2021-10-28 22:49:11 +0300

    logic useQuery class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import React, { Component } from 'react'
+import { useQuery, gql } from '@apollo/client'
 
-import './App.css';
-
-function App() {
-  return (
-    <></>
-  );
+function Query(props) {
+  return props.children(useQuery(props.keyName, {variables:{ input : {title : 'clothes'}}}));
 }
-
-export default App;
+export default class App extends Component{
+  render(){
+    const Data = gql`
+      query Query($input: CategoryInput) {
+  category(input: $input) {
+    name
+    products {
+      name
+    }
+  }
+}
+    `;
+    return(
+      <Query keyName={Data}>
+        {({ data, loading }) => {
+          if (loading) return <h1>Loading</h1>;
+          console.log(data)
+          return (
+            <div className="App">
+              <h1>Hello </h1>
+              <h2>Start editing to see some magic happen!</h2>
+            </div>
+          );
+        }}
+      </Query>
+    )
+  }
+}
